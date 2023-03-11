4c47a7bd30ec8b465b9a9b3daa6dc235974944b5
Gitcatmeoww <queenie.lin0323@gmail.com>
2023-03-11 13:59:12 -0800

    add check tourID middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,18 @@ const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+    console.log(`Tour id is: ${val}`);
+
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'invalid id'
+        });
+    }
+    next();
+}
+
 exports.getAllTours = (req, res) => {
     // console.log(req.requestTime);
     res.status(200).json({
@@ -17,23 +29,8 @@ exports.getAllTours = (req, res) => {
 
 exports.getTour = (req, res) => {
     // console.log(req.params);
-    const id = req.params.id * 1; // convert string to number
-
-    // if (id > tours.length || id < 0) {
-    //     return res.status(404).json({
-    //         status: 'fail',
-    //         message: 'invalid id'
-    //     });
-    // }
-
+    const id = req.params.id * 1;
     const tour = tours.find(el => el.id === id);
-
-    if (!tour) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'invalid id'
-        });
-    }
 
     res.status(200).json({
         status: 'success',
@@ -61,13 +58,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-    if (req.params.id * 1 > tours.length || req.params.id * 1 < 0) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'invalid id'
-        });
-    }
-
     res.status(200).json({
         status: 'success',
         data: {
@@ -77,12 +67,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-    if (req.params.id * 1 > tours.length || req.params.id * 1 < 0) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'invalid id'
-        });
-    }
     // 204: no content
     res.status(204).json({
         status: 'success',
