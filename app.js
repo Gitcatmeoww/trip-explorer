a04ef66dcd771671c1e648f0294f26ecc3bb7409
Gitcatmeoww <queenie.lin0323@gmail.com>
2023-03-17 12:06:41 -0700

    connect w/ mongoDB

--- app.js
+++ app.js
@@ -4,14 +4,13 @@ const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
-
 const app = express();
 
 /**
  * MIDDLEWARES
  */
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'))
+    app.use(morgan('dev'));
 };
 
 app.use(express.json());
--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+    .then(() => {
+        console.log('DB connection established!')
+    });
 
 // console.log(process.env);
 
