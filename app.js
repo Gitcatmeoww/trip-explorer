41c7e6490ec5013d3eb7df88adde93853c0bfc81
Gitcatmeoww <queenie.lin0323@gmail.com>
2023-05-20 17:04:55 -0700

    refactor global error handling

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -31,11 +33,10 @@ app.use('/api/v1/users', userRouter);
 
 // Handle undefined routes
 app.all('*', (req, res, next) => {
-    res.status(404).json({
-        status: 'fail',
-        message: `Can't find ${req.originalUrl} on this server!`
-    })
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 })
+
+app.use(globalErrorHandler);
 
 /**
  * START SERVER
